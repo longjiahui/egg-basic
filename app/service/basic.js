54d7c4a78d8972a9f45ac4b5b0b6e467e9af1493
anfo <longjiahui@hotmail.com>
2022-10-18 20:54:02 +0800

    f

---
+++ app/service/basic.js
@@ -0,0 +1,116 @@
+module.exports = app=>class extends app.Service{
+    getPageProps(){
+        let ctx = this.ctx
+        let {
+            page,
+            pageSize
+        } = ctx.request.body || {}
+
+        page = page?+page:1
+        pageSize = pageSize?+pageSize:20
+        return {
+            page,
+            pageSize,
+        }
+    }
+
+    getLimitAndSkip(){
+        let { page, pageSize } = this.getPageProps()
+        return {
+            limit: pageSize,
+            skip: (page-1) * pageSize,
+        }
+    }
+
+    async pageData(options){
+        let {
+            model, 
+            queryHandler = query=>query,
+            dataHandler = data=>data,
+            condition = {},
+            projection,
+
+            // 互斥 只有一个生效
+            withDeleted = false,
+            onlyDeleted = false,
+            
+        } = options || {}
+
+        let findFunc = model?.find, countDocumentsFunc = model?.countDocuments
+        if(withDeleted){
+            findFunc = model?.findWithDeleted
+            countDocumentsFunc = modal?.countDocumentsWithDeleted
+        }
+        if(onlyDeleted){
+            findFunc = model?.findDeleted
+            countDocumentsFunc = model?.countDocumentsDeleted
+        }
+
+        // 必须指定model为this
+        let datas = await (queryHandler(findFunc?.call(model, condition, projection, this.getLimitAndSkip()))?.exec())
+        let total = await countDocumentsFunc?.call(model, condition)
+        return this.makePageData(total, 
+            (dataHandler?(await dataHandler?.(datas)):datas) || [],)
+    }
+
+    makePageData(total, data){
+        return {
+            data,
+            total
+        }
+    }
+
+    emptyPageData(){
+        return this.makePageData(0, [])
+    }
+
+    filterData(data, fields = ''){
+        fields = fields.trim()
+        let items = fields.split(' ')
+        if(fields?.[0] === '-'){
+            items.forEach(i=>{
+                delete data[i.slice(1)]
+            })
+        }else{
+            data = items.reduce((t, i)=>{
+                if(data.hasOwnProperty(i)){
+                    t[i] = data[i]
+                }
+                return t
+            }, {})
+        }
+        return data
+    }
+
+    async save(options){
+        let {
+            model,
+            dataHandler = data=>data,
+            dataHandlerWhenUpdate = data=>({$set: data}),
+            dataHandlerWhenSave = data=>data,
+            session,
+            // e.g.: '-password' / 'test testa testb'
+            fields = null,
+            upsert = false,
+
+            data,
+        } = options || {}
+        let ctx = this.ctx
+        
+        data = data || ctx.request.body
+        let { _id } = data
+        data = this.filterData(data, fields)
+        data = dataHandler(data)
+        if(_id){
+            data = dataHandlerWhenUpdate(data)
+            return await model.findByIdAndUpdate(_id, data, {
+                session,
+                new: true,
+                upsert,
+            })
+        }else{
+            data = dataHandlerWhenSave(data)
+            return (await model.create([data], { session }))?.[0]
+        }
+    }
+}
--- app/service/ret.js
+++ app/service/ret.js
@@ -24,112 +24,19 @@ module.exports = app=>class extends app.Service{
         }
     }
 
-    getPageProps(){
-        let ctx = this.ctx
-        let {
-            page,
-            pageSize
-        } = ctx.request.body || {}
-
-        page = page?+page:1
-        pageSize = pageSize?+pageSize:20
-        return {
-            page,
-            pageSize,
-        }
+    async pageData(...rest){
+        return this.success(await this.ctx.service.basic.pageData(...rest))
     }
 
-    getLimitAndSkip(){
-        let { page, pageSize } = this.getPageProps()
-        return {
-            limit: pageSize,
-            skip: (page-1) * pageSize,
-        }
-    }
-
-    async pageData(options){
-        let {
-            model, 
-            queryHandler = query=>query,
-            dataHandler = data=>data,
-            condition = {},
-            projection,
-
-            // 互斥 只有一个生效
-            withDeleted = false,
-            onlyDeleted = false,
-            
-        } = options || {}
-
-        let findFunc = model?.find, countDocumentsFunc = model?.countDocuments
-        if(withDeleted){
-            findFunc = model?.findWithDeleted
-            countDocumentsFunc = modal?.countDocumentsWithDeleted
-        }
-        if(onlyDeleted){
-            findFunc = model?.findDeleted
-            countDocumentsFunc = model?.countDocumentsDeleted
-        }
-
-        // 必须指定model为this
-        let datas = await (queryHandler(findFunc?.call(model, condition, projection, this.getLimitAndSkip()))?.exec())
-
-        let total = await countDocumentsFunc?.call(model, condition)
-        return this.makePageData(total, 
-            (dataHandler?(await dataHandler?.(datas)):datas) || [],)
-    }
-
-    makePageData(total, data){
-        return this.success({
-            data,
-            total
-        })
+    makePageData(...rest){
+        return this.success(this.ctx.service.basic.makePageData(...rest))
     }
 
     emptyPageData(){
         return this.makePageData(0, [])
     }
 
-    async save(options){
-        let {
-            model,
-            dataHandler = data=>data,
-            dataHandlerWhenUpdate = data=>data,
-            dataHandlerWhenSave = data=>data,
-            session,
-            // e.g.: '-password' / 'test testa testb'
-            fields = null,
-        } = options || {}
-        let ctx = this.ctx
-        
-        let data = ctx.request.body
-        let { _id } = data
-        if(fields){
-            fields = fields.trim()
-            let items = fields.split(' ')
-            if(fields?.[0] === '-'){
-                items.forEach(i=>{
-                    delete data[i.slice(1)]
-                })
-            }else{
-                data = items.reduce((t, i)=>{
-                    if(data.hasOwnProperty(i)){
-                        t[i] = data[i]
-                    }
-                    return t
-                }, {})
-            }
-        }
-        data = dataHandler(data)
-        if(_id){
-            data = dataHandlerWhenUpdate(data)
-            return this.success(await model.findByIdAndUpdate(_id, data, {
-                session,
-                new: true
-            }))
-        }else{
-            data = dataHandlerWhenSave(data)
-            return this.success((await model.create([data], { session }))?.[0])
-        }
+    async save(...rest){
+        return this.success(await this.ctx.service.basic.save(...rest))
     }
 }
